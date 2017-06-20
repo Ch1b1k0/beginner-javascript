108a10aa8730ceb2c57dd4edd0243d04856e8b13
Ch1b1k0 <baileykanoe@hawaii.rr.com>
2017-06-19 14:59:59 -1000

    not finished

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 function fortuneTeller(){
-	console.log("hello");
+	var numOfChildren = 500000000000;
+	var partnerName = "John"
+	var Location = "chibitian"
+	var job="psychiatrist"
+	console.log("You will be a X in Y, and married to Z with N kids.");
+	console.log("You will be a" + job + "in" + Location +", and married to" + partnerName + "with" + numOfChildren + "kids.");
 }
 
-function ageCalculator(){
+fortuneTeller();
 
+function ageCalculator(){
+	var birthYear = 10, 13, 2003
+	var futureYear = 8, 19, 2167
 }
